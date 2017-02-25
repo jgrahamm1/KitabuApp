4735aeb8027e6f3a73a032e3356a54aa48be3171
jgrahamm1 <jack.m.graham@gmail.com>
2017-02-24 20:08:29 -0500

    WelcomeActivity ready, RegistrationActivity in progress.

--- app/src/main/java/com/example/jgraham/kitabureg1/LoginActivity.java
+++ app/src/main/java/com/example/jgraham/kitabureg1/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.example.jgraham.kitabureg1;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by jgraham on 2/24/17.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
--- app/src/main/java/com/example/jgraham/kitabureg1/WelcomeActivity.java
+++ app/src/main/java/com/example/jgraham/kitabureg1/WelcomeActivity.java
@@ -1,8 +1,53 @@
 package com.example.jgraham.kitabureg1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by jgraham on 2/24/17.
  */
 
-public class WelcomeActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // Buttons
+    protected Button login_button;
+    protected Button register_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Register listener for buttons
+        login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(this);
+
+        register_button = (Button) findViewById(R.id.register_button);
+        register_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Determine if user pressed 'Login' or 'Register'
+        switch(v.getId()) {
+
+            // Login Button
+            case R.id.login_button:
+                // TODO start LoginActivity
+                break;
+
+            // Register Button
+            case R.id.register_button:
+                Intent intent = new Intent(v.getContext(), RegistrationActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                Log.d("ERROR", "WelcomeActivity OnClickListener does not recognize this View");
+        }
+    }
 }
