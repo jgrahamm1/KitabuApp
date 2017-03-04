bb7c24d4be53fd67e73c6de22e439c1da32ae15e
Mahesh Devalla <maheshdevalla@maheshs-air.wireless.dartmouth.edu>
2017-03-04 18:32:08 -0500

    added list to notifications tab

--- app/src/main/java/com/example/jgraham/kitabureg1/Tab3.java
+++ app/src/main/java/com/example/jgraham/kitabureg1/Tab3.java
@@ -1,15 +1,20 @@
 package com.example.jgraham.kitabureg1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
+import com.example.jgraham.kitabureg1.database.KitabuEntry;
 import com.example.jgraham.kitabureg1.database.MySQLiteDbHelper;
+
+import java.util.List;
 
 /**
  * Created by maheshdevalla on 2/24/17.
@@ -18,17 +23,40 @@ import com.example.jgraham.kitabureg1.database.MySQLiteDbHelper;
 public class Tab3 extends Fragment{
     private static MySQLiteDbHelper mySQLiteDbHelper;
     ListView listview;
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Log.d("Tab3","Tab3");
-        View rootView = inflater.inflate(R.layout.tab3, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        return rootView;
-    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mySQLiteDbHelper = new MySQLiteDbHelper(getContext());
+        View rootView = inflater.inflate(R.layout.tab3, container, false);
+        List<KitabuEntry> values = mySQLiteDbHelper.fetchEntries();
+        final ArrayAdapter<KitabuEntry> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_expandable_list_item_1,values);
+        ListView lv= (ListView) rootView.findViewById(R.id.datalisttab3);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("position",String.valueOf(position));
+                KitabuEntry ke = adapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DeleteActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putLong("RowID",ke.getmRowID());
+                bundle.putInt("id",ke.getmId());
+                bundle.putString("title",ke.getmTitle());
+                bundle.putString("link",ke.getmLink());
+                bundle.putString("tags",ke.getmTags());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+        return rootView;
     }
 
 }
