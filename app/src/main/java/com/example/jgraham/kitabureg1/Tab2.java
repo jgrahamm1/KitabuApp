6b745c8922816567a82f63e89d0e3966ec46b822
Mahesh Devalla <maheshdevalla@maheshs-air.wireless.dartmouth.edu>
2017-03-04 19:27:01 -0500

    added lisview in tab2

--- app/src/main/java/com/example/jgraham/kitabureg1/Tab2.java
+++ app/src/main/java/com/example/jgraham/kitabureg1/Tab2.java
@@ -7,36 +7,56 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.jgraham.kitabureg1.database.KitabuEntry;
+import com.example.jgraham.kitabureg1.database.MySQLiteDbHelper;
+
+import java.util.List;
 
 /**
  * Created by maheshdevalla on 2/24/17.
  */
 
-public class Tab2 extends Fragment implements View.OnClickListener {
+public class Tab2 extends Fragment {
 
+    private static MySQLiteDbHelper mySQLiteDbHelper;
+    ListView listview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        int value=1;
-
-        Log.d("Tab2","Tab2");
+        mySQLiteDbHelper = new MySQLiteDbHelper(getContext());
         View rootView = inflater.inflate(R.layout.tab2, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-
-        // Temporary button, register to listener
-        Button create_btn = (Button) rootView.findViewById(R.id.create_link);
-        create_btn.setOnClickListener(this);
-
+        List<KitabuEntry> values = mySQLiteDbHelper.fetchPublicEntries();
+        final ArrayAdapter<KitabuEntry> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_expandable_list_item_1, values);
+        ListView lv = (ListView) rootView.findViewById(R.id.datalisttab2);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("position", String.valueOf(position));
+                KitabuEntry ke = adapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DeleteActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putLong("RowID", ke.getmRowID());
+                bundle.putInt("id", ke.getmId());
+                bundle.putString("title", ke.getmTitle());
+                bundle.putString("link", ke.getmLink());
+                bundle.putString("tags", ke.getmTags());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
         return rootView;
-    }
-//
-    @Override
-    public void onClick(View v) {
-        Log.d("TAB2", "OnClick called");
-        if (v.getId() == R.id.create_link) {
-            Intent intent = new Intent(v.getContext(), SendDataActivity.class);
-            startActivity(intent);
-        }
     }
 }
