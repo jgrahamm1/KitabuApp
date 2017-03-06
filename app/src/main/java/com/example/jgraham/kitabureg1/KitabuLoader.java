cb847f7e5757e9a949662022d1b9562ed7cfba9f
maheshdevalla <maheshdevalla@gmail.com>
2017-03-06 15:26:10 -0500

    Tabs may work

---
+++ app/src/main/java/com/example/jgraham/kitabureg1/KitabuLoader.java
@@ -0,0 +1,34 @@
+package com.example.jgraham.kitabureg1;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.example.jgraham.kitabureg1.database.KitabuEntry;
+import com.example.jgraham.kitabureg1.database.MySQLiteDbHelper;
+
+import java.util.ArrayList;
+
+/**
+ * Created by maheshdevalla on 3/5/17.
+ */
+
+public class KitabuLoader extends AsyncTaskLoader<ArrayList<KitabuEntry>> {
+
+    Context mContext;
+    public KitabuLoader(Context context)
+    {
+        super(context);
+        mContext = context;
+    }
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public ArrayList<KitabuEntry> loadInBackground() {
+        MySQLiteDbHelper helper = MySQLiteDbHelper.getInstance(getContext());
+        ArrayList<KitabuEntry> values = helper.fetchPrivateEntries();
+        return values;
+    }
+}
