800491701376548e2b62d124484340dc613005ee
maheshdevalla <maheshdevalla@gmail.com>
2017-03-07 21:35:01 -0500

    Disable share button on permission not given.

--- app/src/main/java/com/example/jgraham/kitabureg1/AlarmScheduleActivity.java
+++ app/src/main/java/com/example/jgraham/kitabureg1/AlarmScheduleActivity.java
@@ -16,10 +16,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-/**
- * Created by jgraham on 3/4/17.
- */
-
 public class AlarmScheduleActivity extends AppCompatActivity implements DialogManual.ManualDialogListener {
 
     // Calendar for storing the user's time of notification
@@ -36,15 +32,14 @@ public class AlarmScheduleActivity extends AppCompatActivity implements DialogMa
         // Store ID
         m_id = getIntent().getIntExtra("id", -1);
 
+        // Is id value returned is invalid then don't set alarm
         if (m_id == -1) {
             Log.d("ALARM", "Error getting id of URL (-1)");
             finish();
         }
-
         // Init calendar that user sets time and date for
         m_calendar = Calendar.getInstance();
         m_calendar.setTimeInMillis(System.currentTimeMillis());
-
         ListView list_view = (ListView) findViewById(R.id.alarmschedule_list_view);
         list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -54,7 +49,6 @@ public class AlarmScheduleActivity extends AppCompatActivity implements DialogMa
                 if (position == 0) {
                     DialogFragment dialog_fragment = DialogManual.newInstance(DialogManual.DATE_PICKER_DIALOG);
                     dialog_fragment.show(getFragmentManager(), "date_picker_dialog");
-
                 }
                 // Position 1: Time
                 else if (position == 1) {
@@ -65,19 +59,21 @@ public class AlarmScheduleActivity extends AppCompatActivity implements DialogMa
         });
     }
 
+    // Method to select the Data picker: used references from MyRuns.
     @Override
     public void onDatePickerDialogSet(DialogFragment dialog, int year, int month, int day) {
         Log.d("ALARM", "day: " + day);
         m_calendar.set(year, month, (day));
     }
 
+    // Method to select the Time picker: used references from MyRuns.
     @Override
     public void onTimePickerDialogSet(DialogFragment dialog, int hour, int minute) {
         m_calendar.set(Calendar.HOUR_OF_DAY, hour);
         m_calendar.set(Calendar.MINUTE, minute);
     }
 
-    // Save clicked
+    // Save clicked and schedule the alarm.
     public void scheduleClick(View view) {
         // Handle date field
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
@@ -85,10 +81,9 @@ public class AlarmScheduleActivity extends AppCompatActivity implements DialogMa
         // Schedule alarm based on calendar
         // Calculate the time when it expires.
         Intent intent = new Intent(this, EMAAlarmReceiver.class);
-        // Store ID for Prashant DB
+        // Store ID in the Database
         intent.putExtra("id", m_id);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, m_calendar.getTimeInMillis(), pendingIntent);
         Log.d("ALARM", "Alarm set at " + date_string + " for url with id: " + m_id);
