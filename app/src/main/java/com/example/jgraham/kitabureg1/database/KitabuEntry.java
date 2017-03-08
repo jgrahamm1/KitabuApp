10f881e66dc0f6d235ea43a6d07b0d0914660ad9
maheshdevalla <maheshdevalla@gmail.com>
2017-03-07 21:13:07 -0500

    Disable share button on permission not given.

--- app/src/main/java/com/example/jgraham/kitabureg1/database/KitabuEntry.java
+++ app/src/main/java/com/example/jgraham/kitabureg1/database/KitabuEntry.java
@@ -7,14 +7,15 @@ import org.json.JSONObject;
 
 public class KitabuEntry {
 
-    private Long mRowID; // Unique Row ID for database operations.
-    private int mId;    // Unique id generated from web server.
+    private Long mRowID;       // Unique Row ID for database operations.
+    private int mId;          // Unique id generated from web server.
     private String mLink;     // URL that is shared.
     private String mTitle;    //Title of the URL.
-    private int mType;        // column if is is
-    private String mTags;
-    private Long mPhoneNo;
+    private int mType;        //Column if is is public/private link.
+    private String mTags;     // Tags of the link just for description.
+    private Long mPhoneNo;     // Phone number of the user.
 
+//    Constructor to add the default values if user provided empty records.
     public KitabuEntry() {
         this.mId=0;
         this.mLink="";
@@ -24,6 +25,7 @@ public class KitabuEntry {
         this.mPhoneNo=0L;
     }
 
+    //    Constructor to add the user provided values
     public KitabuEntry(String id_str, String url, String phone, String tags, int type, String title) {
         this.mId = Integer.parseInt(id_str);
         this.mLink = url;
@@ -33,26 +35,32 @@ public class KitabuEntry {
         this.mType = type;
     }
 
+    // Get the RowID of the table ** if needed**
     public Long getmRowID() {
         return mRowID;
     }
 
+    // Set the RowID of the table ** if needed**
     public void setmRowID(Long mRowID) {
         this.mRowID = mRowID;
     }
 
+   // Get the id generated from the server
     public int getmId() {
         return mId;
     }
 
+    // set the id generated from the server
     public void setmId(int mId) {
         this.mId = mId;
     }
 
+    // Get the link or URL
     public String getmLink() {
         return mLink;
     }
 
+    // Set the link or URL
     public void setmLink(String mLink) {
         this.mLink = mLink;
     }
@@ -61,38 +69,42 @@ public class KitabuEntry {
         return mTitle;
     }
 
+    // Set the title for the URL
     public void setmTitle(String mTitle) {
         this.mTitle = mTitle;
     }
 
+    // Get the title for the URL
     public int getmType() {
         return mType;
     }
-
+    // Set the type if public or private
     public void setmType(int mType) {
         this.mType = mType;
     }
-
+    // Get the Tags based upon the URL
     public String getmTags() {
         return mTags;
     }
-
+    // Set the Tags based upon the URL
     public void setmTags(String mTags) {
         this.mTags = mTags;
     }
-
+    // Get the phone number who have sent.
     public Long getmPhoneNo() {
         return mPhoneNo;
     }
-
+    // Set the phone number who have sent.
     public void setmPhoneNo(Long mPhoneNo) {
         this.mPhoneNo = mPhoneNo;
     }
+    // Method to return the values accordinglt in the list view.
     public String toString() {
 
         return this.getmTitle() +" \n " + getmLink()+" \n "+getmPhoneNo();
     }
 
+    // Convert the object to JSON object.
     public JSONObject toJSONObject() {
         JSONObject obj = new JSONObject();
 
@@ -109,6 +121,7 @@ public class KitabuEntry {
         }
         return obj;
     }
+    // Based on the JSON object received from server add the data to the local MYSQL DB.
     public KitabuEntry(JSONObject jsonObject)
     {
         try {
@@ -130,9 +143,7 @@ public class KitabuEntry {
                 this.mType = 1;
             else
                 this.mType = 0;
-
             Log.d("type", String.valueOf(this.mType));
-           // this.mTags = jsonObject.getString("tag_list");
         }
         catch (JSONException e)
         {
